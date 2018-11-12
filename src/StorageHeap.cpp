870e787988d42094de5dfdac9cbbb6578c4d7789
SevenSwen <IntegralTeam>
2018-11-12 19:00:42 +0700

    add storage heap mutex

--- src/StorageHeap.cpp
+++ src/StorageHeap.cpp
@@ -6,17 +6,25 @@ void StorageHeap::AddChunk(const std::string& path, uint64_t size)
     chunk.path = path;
     chunk.totalSpace = size;
     chunk.freeSpace = 0;
-    chunks.push_back(chunk);
+    cs_dfs.lock();
+    {
+        chunks.push_back(chunk);
+    }
+    cs_dfs.unlock();
 }
 
 void StorageHeap::FreeChunk(const std::string& path)
 {
-    for (auto it = chunks.begin(); it != chunks.end(); ++it) {
-        if (it->path == path) {
-            chunks.erase(it);
-            break;
+    cs_dfs.lock();
+    {
+        for (auto it = chunks.begin(); it != chunks.end(); ++it) {
+            if (it->path == path) {
+                chunks.erase(it);
+                break;
+            }
         }
     }
+    cs_dfs.unlock();
 }
 
 std::vector<StorageChunk> StorageHeap::GetChunks() const
@@ -40,7 +48,11 @@ void StorageHeap::SetPubKey(const std::string& uri, const std::string& pubkey)
     for (auto&& chunk : chunks) {
         for (auto&& file : chunk.files) {
             if (file.uri == uri) {
-                file.pubkey = pubkey;
+                cs_dfs.lock();
+                {
+                    file.pubkey = pubkey;
+                }
+                cs_dfs.unlock();
                 return;
             }
         }
@@ -65,8 +77,12 @@ boost::optional<AllocatedFile> StorageHeap::AllocateFile(const std::string& chun
                 file.size = size;
                 file.uri = uri;
                 // add file to heap
-                files[uri] = file;
-                chunk.freeSpace -= size;
+                cs_dfs.lock();
+                {
+                    files[uri] = file;
+                    chunk.freeSpace -= size;
+                }
+                cs_dfs.unlock();
                 return file;
             }
             return {};
@@ -81,8 +97,12 @@ void StorageHeap::FreeFile(const std::string& uri)
         auto&& _files = chunk.files;
         for (auto it = _files.begin(); it != _files.end(); ++it) {
             if (it->uri == uri) {
-                _files.erase(it);
-                files.erase(files.find(uri));
+                cs_dfs.lock();
+                {
+                    _files.erase(it);
+                    files.erase(files.find(uri));
+                }
+                cs_dfs.unlock();
                 return;
             }
         }
