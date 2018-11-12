0cbb046e81ad3b27fb75d67f12df989dac3dc60e
SevenSwen <IntegralTeam>
2018-11-12 16:04:57 +0700

    add tests [WIP]

---
+++ src/test/storage_heap.cpp
@@ -0,0 +1,14 @@
+#include "StorageHeap.h"
+
+#define BOOST_TEST_MODULE Lux Lib Crypto Test
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(storage_heap_tests)
+
+BOOST_AUTO_TEST_CASE(acc_orderupgrade)
+{
+    BOOST_CHECK( true );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
